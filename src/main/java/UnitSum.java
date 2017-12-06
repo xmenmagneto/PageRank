7c772d559537f3a63e9d5382bc554f0c3c3fc30d
DengTingchang <dengtingchang@DengTingchangs-MacBook-Pro.local>
2017-12-06 11:48:57 -0600

    setup reducer for the 2nd MapReduce job

--- src/main/java/UnitSum.java
+++ src/main/java/UnitSum.java
@@ -28,7 +28,11 @@ public class UnitSum {
         @Override
         public void reduce(Text key, Iterable<DoubleWritable> values, Context context)
                 throws IOException, InterruptedException {
-
+            double sum = 0;
+            for (DoubleWritable value : values) {
+                sum += value.get();
+            }
+            context.write(key, new DoubleWritable(sum));
         }
     }
 
