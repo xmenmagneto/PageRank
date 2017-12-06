14daee2669f156a26e3ebbb290727a0b3ad68421
DengTingchang <dengtingchang@DengTingchangs-MacBook-Pro.local>
2017-12-06 12:10:00 -0600

    setup arguments for Driver

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -9,14 +9,18 @@ public class Driver {
         //args1: dir of PageRank.txt
         //args2: dir of unitMultiplication result
         //args3: times of convergence
-        String transitionMatrix = args[0];
-        String prMatrix = args[1];
-        String unitState = args[2];
-        int count = Integer.parseInt(args[3]);
-        for(int i=0;  i<count;  i++) {
-            String[] args1 = {transitionMatrix, prMatrix+i, unitState+i};
+        String transitionMatrix = args[0]; //directory of transition.txt
+        String prMatrix = args[1]; // /input/pr0  /input/pr1  /input/pr0
+        String subPR = args[2]; //  /output/subpr
+        int count = Integer.parseInt(args[3]);  //想要迭代多少次
+
+        for(int i = 0;  i < count;  i++) {
+            //transitionMatrix始终不变,可以写死
+
+            String[] args1 = {transitionMatrix, prMatrix + i, subPR + i};
             multiplication.main(args1);
-            String[] args2 = {unitState + i, prMatrix+(i+1)};
+
+            String[] args2 = {subPR + i, prMatrix + (i + 1)};
             sum.main(args2);
         }
     }
